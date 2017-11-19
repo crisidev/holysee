81c350a765b605cda3bcffb1ca7112581d16ecd8
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2017-11-19 15:42:25 +0100

    rework the telegram module in similar fashion to the irc one

--- holysee/src/telegram.rs
+++ holysee/src/telegram.rs
@@ -8,85 +8,87 @@ pub mod client {
     use settings::Settings;
     use message::{Message, TransportType};
     use std::thread;
-    use chan::{Sender};
+    use chan::{Sender, Receiver};
     use self::telegram_bot::Api;
     use self::telegram_bot::types::{ChatId, MessageKind, SendMessage, UpdateKind};
     use self::tokio_core::reactor::Core;
 
-    pub fn new(settings: &Settings, to_int_sender_obj: Sender<Message>) -> Sender<Message> {
-        // TODO fix this hardcoded value
-        let (return_sender, from_int_reader) = chan::sync(100);
+    fn main_to_telegram_loop(from_main_queue: &Receiver<Message>, token: &String, chat_id: i64) {
+        let mut core = Core::new().unwrap();
+        let api = Api::configure(token).build(core.handle());
+        let chat = ChatId::new(chat_id);
+        loop {
+            let current: Option<Message> = from_main_queue.recv();
+            match  current {
+                Some(msg) => {
+                    core.run(api.send(SendMessage::new(chat, msg.format())))
+                        .unwrap();
+                }
+                None => {
+                    info!("No message to read on internal channel");
+                }
+            };
+        }
+    }
 
-        info!("Created telegram client");
-        debug!("Running from configuration: {:?}", settings);
-
-        let token = settings.telegram.token.clone();
-
-
-        thread::spawn(move || {
-            let mut core = Core::new().unwrap();
-            let api = Api::configure(&token).build(core.handle());
-            let future = api.stream().for_each(|update| {
-                match update.kind {
-                    UpdateKind::Message(m) => {
-                        match m.kind {
-                            MessageKind::Text { data, entities } => {
-                                let from: String = m.from
-                                    .clone()
-                                    .and_then(|u| {
-                                        u.username.clone().or_else(|| {
-                                            u.last_name
+    fn telegram_to_main_loop(to_main_queue: &Sender<Message>, token: &String) {
+        let mut core = Core::new().unwrap();
+        let api = Api::configure(&token).build(core.handle());
+        let future = api.stream().for_each(|update| {
+            match update.kind {
+                UpdateKind::Message(m) => {
+                    match m.kind {
+                        MessageKind::Text { data, entities } => {
+                            let from: String = m.from
+                                .clone()
+                                .and_then(|u| {
+                                    u.username.clone().or_else(|| {
+                                        u.last_name
                                             .clone()
                                             .and_then(
                                                 |ln| Some(format!("{:?} {:?}", u.first_name, ln)),
                                             )
                                             .or_else(|| Some(u.first_name.clone()))
-                                        })
                                     })
-                                    .unwrap_or(String::from("unset"));
-                                debug!("entities: {:#?} from: {}", entities, from);
-                                to_int_sender_obj
-                                    .send(Message {
-                                        from_transport: TransportType::Telegram,
-                                        from: from,
-                                        to: String::from("-"),
-                                        text: data,
-                                    })
-                            }
-                            _ => {
-                                debug!("messageKind != text");
-                            }
+                                })
+                                .unwrap_or(String::from("unset"));
+                            debug!("entities: {:#?} from: {}", entities, from);
+                            to_main_queue
+                                .send(Message {
+                                    from_transport: TransportType::Telegram,
+                                    from: from,
+                                    to: String::from("-"),
+                                    text: data,
+                                })
+                        }
+                        _ => {
+                            debug!("messageKind != text");
                         }
                     }
-                    _ => {
-                        debug!("UpdateKind != messate");
-                    }
                 }
-                Ok(())
-            });
-            core.run(future)
-        });
-
-        let chat_id = settings.telegram.chat_id;
-        let token_clone = settings.telegram.token.clone();
-
-        thread::spawn(move || {
-            let mut core = Core::new().unwrap();
-            let api = Api::configure(&token_clone).build(core.handle());
-            let chat = ChatId::new(chat_id);
-            loop {
-                let current: Option<Message> = from_int_reader.recv();
-                match  current {
-                    Some(msg) => {
-                        core.run(api.send(SendMessage::new(chat, msg.format())))
-                            .unwrap();
-                    }
-                    None => {
-                        info!("No message to read on internal channel");
-                    }
-                };
+                _ => {
+                    debug!("UpdateKind != messate");
+                }
             }
+            Ok(())
         });
-        return_sender.clone()
+        core.run(future).unwrap();
+    }
+
+    pub fn new(settings: &Settings, to_main_queue: Sender<Message>) -> Sender<Message> {
+        // TODO fix this hardcoded value
+        let (to_telegram_queue, from_main_queue) = chan::sync(100);
+
+        info!("Created telegram client");
+        debug!("Running from configuration: {:?}", settings);
+
+        let token = settings.telegram.token.clone();
+        let token_clone = settings.telegram.token.clone();
+        let chat_id = settings.telegram.chat_id.clone();
+
+        thread::spawn(move || telegram_to_main_loop(&to_main_queue, &token));
+        thread::spawn(move || main_to_telegram_loop(&from_main_queue, &token_clone, chat_id));
+
+        to_telegram_queue.clone()
     }
 }
