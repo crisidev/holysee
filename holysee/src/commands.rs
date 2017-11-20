1d4b7e16d83f30b7fb94076e2ed57b3bf9ae7552
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2017-11-20 16:11:39 +0100

    implement data dir abstraction fix error handling fmt

--- holysee/src/commands.rs
+++ holysee/src/commands.rs
@@ -4,7 +4,6 @@ extern crate serde_json;
 use message::{Message, TransportType};
 use chan::Sender;
 use std::collections::HashMap;
-use std::fs::File;
 use std::fs::OpenOptions;
 use settings;
 
@@ -112,7 +111,7 @@ impl<'a> KarmaCommand<'a> {
     pub fn new(command_prefix: &'a String, settings: &'a settings::Commands) -> KarmaCommand<'a> {
         KarmaCommand {
             name: String::from("karma"),
-            karma: KarmaCommand::read_database(),
+            karma: KarmaCommand::read_database(&settings.data_dir, "karma"),
             command_prefix: command_prefix,
             data_dir: &settings.data_dir,
         }
@@ -120,41 +119,51 @@ impl<'a> KarmaCommand<'a> {
     pub fn matches_message_text(&self, message: &Message) -> bool {
         let re = Regex::new(
             format!(
-                r"(^{}karma (.*)$|^viva (.*)$|^(\w+)\+\+$|^abbasso (.*)$|^(\w+)\-\-$)",
+                r"(^{}karma (.*)$|^[vV]iva (.*)$|^(\w+)\+\+$|^[aA]bbasso (.*)$|^(\w+)\-\-$)",
                 self.command_prefix
             ).as_ref(),
         ).unwrap();
         re.is_match(&message.text)
     }
 
-    fn read_database() -> HashMap<String, i64> {
+    fn read_database(data_dir: &String, name: &str) -> HashMap<String, i64> {
         // load the current known karma
         // TODO: abstract file name and path
-        match File::open("data/karma.json") {
-            Ok(f) => match serde_json::from_reader(f) {
-                Ok(k) => k,
-                Err(e) => {
-                    error!("cannot load from file: {}", e);
-                    HashMap::new()
-                },
-            },
+        match OpenOptions::new().read(true).open(format!(
+            "{}/{}.json",
+            data_dir,
+            name
+        )) {
+            Ok(file) => {
+                match serde_json::from_reader(file) {
+                    Err(e) => {
+                        error!("cannot deserialize file: {}", e);
+                        HashMap::new()
+                    }
+                    Ok(k) => k,
+                }
+            }
             Err(e) => {
-                error!("cannot open file for reading: {}", e);
+                error!("cannot open file: {}", e);
                 HashMap::new()
-            },
+            }
         }
     }
 
     fn write_database(&self) {
-        match OpenOptions::new().write(true).open("data/karma.json") {
+        match OpenOptions::new().write(true).open(format!(
+            "{}/{}.json",
+            self.data_dir,
+            &self.name
+        )) {
             Ok(file) => {
                 match serde_json::to_writer(file, &self.karma) {
                     Err(e) => error!("cannot serialize file: {}", e),
-                                _ => {},
-                                };
-                            },
-                            Err(e) => error!("cannot open file: {}", e),
-                        };
+                    _ => {}
+                };
+            }
+            Err(e) => error!("cannot open file: {}", e),
+        };
     }
 
     fn get(&self, key: &str) -> String {
@@ -169,9 +178,9 @@ impl<'a> KarmaCommand<'a> {
         for group in cap.iter().skip(1) {
             match group {
                 Some(x) => {
-                        *(self.karma.entry(String::from(x.as_str())).or_insert(0)) += value;
-                        karma_irc = self.get(x.as_str());
-                        self.write_database();
+                    *(self.karma.entry(String::from(x.as_str())).or_insert(0)) += value;
+                    karma_irc = self.get(x.as_str());
+                    self.write_database();
                 }
                 None => continue,
             }
@@ -183,8 +192,9 @@ impl<'a> KarmaCommand<'a> {
 impl<'a> Command for KarmaCommand<'a> {
     fn execute(&mut self, msg: &Message, to_irc: &Sender<Message>, to_telegram: &Sender<Message>) {
         debug!("karma execute");
-        let re_get = Regex::new(format!(r"^(?:{})karma (.*)$", self.command_prefix).as_ref()).unwrap();
-        let re_increase =  Regex::new(r"^[vV]iva (.*)$|^(\w+)\+\+$").unwrap();
+        let re_get = Regex::new(format!(r"^(?:{})karma (.*)$", self.command_prefix).as_ref())
+            .unwrap();
+        let re_increase = Regex::new(r"^[vV]iva (.*)$|^(\w+)\+\+$").unwrap();
         let re_decrease = Regex::new(r"^[aA]bbasso (.*)$|^(\w+)\-\-$").unwrap();
 
         let mut karma_irc = String::new();
