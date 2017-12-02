6899828a50b4f9498ac200306454181f685b4a3f
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2017-12-02 20:17:55 +0100

    remove now irrelevant tests

--- holysee/src/commands/mod.rs
+++ holysee/src/commands/mod.rs
@@ -7,34 +7,7 @@ pub mod url_preview;
 pub mod usage;
 
 
-// DISABLED DUE TO REFACTOR
-//#[cfg(test)]
-//mod tests {
-//    use commands::command_dispatcher::CommandDispatcher;
-//    use settings::Settings;
-//
-//    #[test]
-//    fn is_command_enabled_ok() {
-//        let settings = match Settings::new(false) {
-//            Ok(s) => s,
-//            Err(e) => {
-//                panic!("Error accessing config file: {}", e);
-//            }
-//        };
-//        let command_dispatcher = CommandDispatcher::new(&settings.commands);
-//        assert!(command_dispatcher.is_command_enabled("relay"));
-//    }
-//
-//    #[test]
-//    #[should_panic]
-//    fn is_command_enabled_fail() {
-//        let settings = match Settings::new(false) {
-//            Ok(s) => s,
-//            Err(e) => {
-//                panic!("Error accessing config file: {}", e);
-//            }
-//        };
-//        let command_dispatcher = CommandDispatcher::new(&settings.commands);
-//        assert!(command_dispatcher.is_command_enabled("karma"));
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    // Old tests removed due to refactor. New tests will be needed after the refactor is completed.
+}
