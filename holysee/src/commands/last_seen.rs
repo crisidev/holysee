9ee1652c462de64dfb2a8cf8e3bc08a15066f3d1
Bigo <bigo@crisidev.org>
2017-12-03 18:29:25 +0000

    Initial implementation of last_seen tests

--- holysee/src/commands/last_seen.rs
+++ holysee/src/commands/last_seen.rs
@@ -46,17 +46,22 @@ impl<'a> LastSeenCommand<'a> {
         })
     }
 
-    fn write_database(&self) {
+    fn write_database(&self) -> bool {
         let filename = format!("{}/{}.json", self.data_dir, &self.get_name());
         let filename_clone = filename.clone();
         match OpenOptions::new().write(true).truncate(true).open(filename) {
             Ok(file) => {
                 if let Err(e) = serde_json::to_writer(file, &self.last_seen) {
-                    error!("Cannot serialize file {}: {}", filename_clone, e)
+                    error!("Cannot serialize file {}: {}", filename_clone, e);
+                    return false;
                 };
             }
-            Err(e) => error!("Cannot open file {}: {}", filename_clone, e),
+            Err(e) => {
+                error!("Cannot open file {}: {}", filename_clone, e);
+                return false;
+            }
         };
+        true
     }
 
     fn get(&self, key: &str) -> String {
@@ -78,6 +83,21 @@ impl<'a> LastSeenCommand<'a> {
         )) = Local::now().timestamp();
         self.write_database();
     }
+
+    fn handle(&mut self, text: &str, from: &str) -> String {
+        let re_get = Regex::new(
+            format!(r"^(?:{})seen\s+(.*)$", &self.command_prefix).as_ref(),
+        ).unwrap();
+
+        let mut result = String::new(); 
+        // COMMAND HANDLING
+        self.see(from);
+        for cap in re_get.captures_iter(text) {
+            debug!("Last seen captures {:#?}", cap);
+            result = self.get(&cap[1]);
+        }
+        result
+    }
 }
 
 impl<'a> Command for LastSeenCommand<'a> {
@@ -87,15 +107,8 @@ impl<'a> Command for LastSeenCommand<'a> {
         to_irc: &Sender<Message>,
         to_telegram: &Sender<Message>,
     ) {
-        let re_get = Regex::new(
-            format!(r"^(?:{})seen\s+(.*)$", &self.command_prefix).as_ref(),
-        ).unwrap();
-
-        // COMMAND HANDLING
-        self.see(&msg.from);
-        for cap in re_get.captures_iter(&msg.text) {
-            debug!("Last seen captures {:#?}", cap);
-            let last_seen_irc = self.get(&cap[1]);
+        let last_seen_irc = self.handle(&msg.text, &msg.from);
+        if last_seen_irc != "" {
             let last_seen_telegram = last_seen_irc.clone();
             let destination = match msg.to {
                 DestinationType::Channel(ref c) => DestinationType::Channel(c.clone()),
@@ -148,5 +161,54 @@ command. Note that all timestamps are relative to the server's timezone, usually
 
     fn stop_processing(&self) -> bool {
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate tempdir;
+
+    use self::tempdir::TempDir;
+
+    use super::LastSeenCommand;
+
+    #[test]
+    fn test_read_database() {
+        assert!(LastSeenCommand::read_database("adir", "last_seen.json").is_err());
+        let data_dir = TempDir::new("holysee_last_seen").unwrap();
+        assert!(
+            LastSeenCommand::read_database(data_dir.path().to_str().unwrap(), "last_seen.json").is_err()
+        );
+    }
+
+    #[test]
+    fn test_write_database() {
+        // TODO: handle also successful case which now returns
+        // Cannot open file /var/folders/xj/8kykppps3b9d79m8g40nbyz9p52bt_/T/holysee_last_seen.C6rvI2j6eqIa/last_seen.json: No such file or directory (os error 2)
+        // Cannot open file /var/folders/xj/8kykppps3b9d79m8g40nbyz9p52bt_/T/holysee_last_seen.C6rvI2j6eqIa/last_seen.json: No such file or directory (os error 2)
+        let prefix = String::from("!");
+        let karma = LastSeenCommand::new(&prefix, "adir");
+        assert!(!karma.write_database());
+    }
+
+    #[test]
+    fn test_handle() {
+        let prefix = String::from("!");
+        let data_dir = TempDir::new("holysee_last_seen").unwrap();
+        let mut seen = LastSeenCommand::new(&prefix, data_dir.path().to_str().unwrap());
+
+        let cases = [
+            ["!seen", "auser"],
+            ["!Seen", "auser"],
+            ["!seen", "anotheruser"],
+            ["!Seen", "anotheruser"],
+        ];
+
+        for case in cases.iter() {
+            println!("{}", seen.handle(case[0], case[1]));
+            assert!(seen.handle(case[0], case[1]).is_empty());
+        }
+
+        // TODO: actually implement testing when a user is seen
     }
 }
